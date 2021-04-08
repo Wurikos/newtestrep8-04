068780c955eae3114aa233a6b8736a1647538d20
Sulin Alexander <50768680+Wurikos@users.noreply.github.com>
2021-04-08 11:58:53 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,35 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"606ec578f11f97001e815941",
+				"606ec578f11f97001e815943",
+				"606ec5a9ff9331001f9ee95f"
+			]
+		},
+		"606ec578f11f97001e815941": {
+			"id": "606ec578f11f97001e815941",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"606ec578f11f97001e815943": {
+			"id": "606ec578f11f97001e815943",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"606ec5a9ff9331001f9ee95f": {
+			"id": "606ec5a9ff9331001f9ee95f",
+			"pageUrl": "page",
+			"name": "page"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
